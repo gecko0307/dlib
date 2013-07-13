aa7b1a24ce9a9a1bac3978ee65403e6b104b99e8
Andrey Penechko <mrsmith33@yandex.ru>
2013-07-13 15:39:15 +0300

    Fix errors while compiling with dub build.
    Fix: wrong file names.
    Fix: replace LF by CRLF.
    Fix: allow warnings in dub config.
    Fix: little hack to workaround rvalue refences issue in dlib.image.filters.morphology.
    Add: Signal2D.divide(Signal2D img), needs cheking.

--- dlib/geometry/triangle.d
+++ dlib/geometry/triangle.d
@@ -26,7 +26,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-module dlib.mesh.triangle;
+module dlib.geometry.triangle;
 
 private
 {
--- dlib/image/io/png.d
+++ dlib/image/io/png.d
@@ -26,7 +26,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-module dlib.image.png;
+module dlib.image.io.png;
 
 private
 {
