1569a9075d7f7ce132eaf932bc5d4b15e1bc9128
Timur Gafarov <gecko0307@gmail.com>
2013-12-12 23:00:36 +0300

    Added package.d for dlib.math

---
+++ dlib/math/package.d
@@ -0,0 +1,17 @@
+module dlib.math;
+
+public
+{
+    import dlib.math.affine;
+    import dlib.math.dual;
+    import dlib.math.interpolation;
+    import dlib.math.vector;
+    import dlib.math.complex;
+    import dlib.math.fft;
+    import dlib.math.linear;
+    import dlib.math.quaternion;
+    import dlib.math.diff;
+    import dlib.math.fixed;
+    import dlib.math.matrix;
+    import dlib.math.utils;
+}
