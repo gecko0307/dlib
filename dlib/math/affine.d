9b646463e3a71ed1e6b39245ac5f3b15409990d6
Timur Gafarov <gecko0307@gmail.com>
2014-01-31 10:05:45 +0300

    Fixed #16

--- dlib/math/affine.d
+++ dlib/math/affine.d
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013 Timur Gafarov 
+Copyright (c) 2013-2014 Timur Gafarov 
 
 Boost Software License - Version 1.0 - August 17th, 2003
 
@@ -46,64 +46,9 @@ import dlib.math.matrix;
  * dlib uses 4x4 matrices to represent affine transformations.
  */
 
-/*
- * Setup a rotation matrix, given XYZ rotation angles
- */
-Matrix!(T,4) fromEuler(T) (Vector!(T,3) v)
-{
-    auto res = Matrix!(T,4).identity;
-    
-    T cx = cos(v.x);
-    T sx = sin(v.x);
-    T cy = cos(v.y);
-    T sy = sin(v.y);
-    T cz = cos(v.z);
-    T sz = sin(v.z);
+// TODO: fromEuler
 
-    T sxsy = sx * sy;
-    T cxsy = cx * sy;
-
-    res.a11 =  (cy * cz);
-    res.a12 =  (sxsy * cz) + (cx * sz);
-    res.a13 = -(cxsy * cz) + (sx * sz);
-
-    res.a21 = -(cy * sz);
-    res.a22 = -(sxsy * sz) + (cx * cz);
-    res.a23 =  (cxsy * sz) + (sx * cz);
-
-    res.a31 =  (sy);
-    res.a32 = -(sx * cy);
-    res.a33 =  (cx * cy);
-
-    return res;
-}
-
-/*
- * Setup the euler angles in radians, given a rotation matrix
- */
-Vector!(T,3) toEuler(T) (Matrix!(T,4) m)
-body
-{
-    Vector!(T,3) v;
-
-    v.y = asin(m.a31);
-
-    T cy = cos(v.y);
-    T oneOverCosY = 1.0 / cy;
-
-    if (fabs(cy) > 0.001)
-    {
-        v.x = atan2(-m.a32 * oneOverCosY, m.a33 * oneOverCosY);
-        v.z = atan2(-m.a21 * oneOverCosY, m.a11 * oneOverCosY);
-    }
-    else
-    {
-        v.x = 0.0;
-        v.z = atan2(m.a12, m.a22);
-    }
-
-    return v;
-}
+// TODO: toEuler
 
 /*
  * Right vector of the matrix
@@ -111,7 +56,7 @@ body
 Vector!(T,3) right(T) (Matrix!(T,4) m)
 body
 {
-    return Vector!(T,3)(m.a11, m.a12, m.a13);
+    return Vector!(T,3)(m.a11, m.a21, m.a31);
 }
 
 /*
@@ -120,7 +65,7 @@ body
 Vector!(T,3) up(T) (Matrix!(T,4) m)
 body
 {
-    return Vector!(T,3)(m.a21, m.a22, m.a23);
+    return Vector!(T,3)(m.a12, m.a22, m.a32);
 }
 
 /*
@@ -129,7 +74,7 @@ body
 Vector!(T,3) forward(T) (Matrix!(T,4) m)
 body
 {
-    return Vector!(T,3)(m.a31, m.a32, m.a33);
+    return Vector!(T,3)(m.a13, m.a23, m.a33);
 }
 
 /*
@@ -138,7 +83,7 @@ body
 Vector!(T,3) translation(T) (Matrix!(T,4) m)
 body
 {
-    return Vector!(T,3)(m.a41, m.a42, m.a43);
+    return Vector!(T,3)(m.a14, m.a24, m.a34);
 }
 
 /*
@@ -151,10 +96,10 @@ body
 }
 
 /* 
- * Create a matrix to perform a rotation about an arbitrary axis
+ * Create a matrix to perform a rotation about a world axis
  * (theta in radians)
  */
-Matrix!(T,4) rotationMatrix(T) (int rotaxis, T theta)
+Matrix!(T,4) rotationMatrix(T) (uint rotaxis, T theta)
 body
 {
     auto res = Matrix!(T,4).identity;
@@ -165,21 +110,21 @@ body
     switch (rotaxis)
     {
         case Axis.x:
-            res.a11 = 1.0; res.a21 = 0.0; res.a31 = 0.0;
-            res.a12 = 0.0; res.a22 = c;   res.a32 = -s;
-            res.a13 = 0.0; res.a23 = s;   res.a33 =  c;
+            res.a11 = 1.0; res.a12 = 0.0; res.a13 = 0.0;
+            res.a21 = 0.0; res.a22 = c;   res.a23 =  s;
+            res.a31 = 0.0; res.a32 = -s;  res.a33 =  c;
             break;
 
         case Axis.y:
-            res.a11 = c;   res.a21 = 0.0; res.a31 = s;
-            res.a12 = 0.0; res.a22 = 1.0; res.a32 = 0.0;
-            res.a13 = -s;  res.a23 = 0.0; res.a33 = c;
+            res.a11 = c;   res.a12 = 0.0; res.a13 = -s;
+            res.a21 = 0.0; res.a22 = 1.0; res.a23 = 0.0;
+            res.a31 = s;   res.a32 = 0.0; res.a33 = c;
             break;
 
         case Axis.z:
-            res.a11 = c;   res.a21 = -s;  res.a31 = 0.0;
-            res.a12 = s;   res.a22 =  c;  res.a32 = 0.0;
-            res.a13 = 0.0; res.a23 = 0.0; res.a33 = 1.0;
+            res.a11 = c;   res.a12 =  s;  res.a13 = 0.0;
+            res.a21 = -s;  res.a22 =  c;  res.a23 = 0.0;
+            res.a31 = 0.0; res.a32 = 0.0; res.a33 = 1.0;
             break;
 
         default:
@@ -196,9 +141,9 @@ Matrix!(T,4) translationMatrix(T) (Vector!(T,3) v)
 body
 {
     auto res = Matrix!(T,4).identity;
-    res.a41 = v.x;
-    res.a42 = v.y;
-    res.a43 = v.z;
+    res.a14 = v.x;
+    res.a24 = v.y;
+    res.a34 = v.z;
     return res;
 }
 
@@ -234,7 +179,7 @@ body
 
     res.a11 = (ax * scaleAxis.x) + 1.0;
     res.a22 = (ay * scaleAxis.y) + 1.0;
-    res.a32 = (az * scaleAxis.z) + 1.0;
+    res.a33 = (az * scaleAxis.z) + 1.0;
 
     res.a12 = res.a21 = (ax * scaleAxis.y);
     res.a13 = res.a31 = (ax * scaleAxis.z);
@@ -245,8 +190,10 @@ body
 
 /*
  * Setup the matrix to perform a shear
+ *
+ * NOTE: needs test
  */
-Matrix!(T,4) shearMatrix(T) (int shearAxis, T s, T t)
+Matrix!(T,4) shearMatrix(T) (uint shearAxis, T s, T t)
 body
 {
     auto res = Matrix!(T,4).identity;
@@ -254,21 +201,21 @@ body
     switch (shearAxis)
     {
         case Axis.x:
-            res.a11 = 1.0; res.a21 = 0.0; res.a31 = 0.0;
-            res.a12 = s;   res.a22 = 1.0; res.a32 = 0.0;
-            res.a13 = t;   res.a23 = 0.0; res.a33 = 1.0;
+            res.a11 = 1.0; res.a12 = 0.0; res.a13 = 0.0;
+            res.a21 = s;   res.a22 = 1.0; res.a23 = 0.0;
+            res.a31 = t;   res.a32 = 0.0; res.a33 = 1.0;
             break;
 
         case Axis.y:
-            res.a11 = 1.0; res.a21 = s;   res.a31 = 0.0;
-            res.a12 = 0.0; res.a22 = 1.0; res.a32 = 0.0;
-            res.a13 = 0.0; res.a23 = t;   res.a33 = 1.0;
+            res.a11 = 1.0; res.a12 = s;   res.a13 = 0.0;
+            res.a21 = 0.0; res.a22 = 1.0; res.a23 = 0.0;
+            res.a31 = 0.0; res.a32 = t;   res.a33 = 1.0;
             break;
 
         case Axis.z:
-            res.a11 = 1.0; res.a21 = 0.0; res.a31 = s;
-            res.a12 = 0.0; res.a22 = 1.0; res.a32 = t;
-            res.a13 = 0.0; res.a23 = 0.0; res.a33 = 1.0;
+            res.a11 = 1.0; res.a12 = 0.0; res.a13 = s;
+            res.a21 = 0.0; res.a22 = 1.0; res.a23 = t;
+            res.a31 = 0.0; res.a32 = 0.0; res.a33 = 1.0;
             break;
 
         default:
@@ -282,6 +229,8 @@ body
  * Setup the matrix to perform a projection onto a plane passing
  * through the origin. The plane is perpendicular to the
  * unit vector n.
+ *
+ * NOTE: needs test
  */
 Matrix!(T,4) projectionMatrix(T) (Vector!(T,3) n)
 in
@@ -303,69 +252,9 @@ body
     return res;
 }
 
-/*
- * Setup the matrix to perform a reflection about a plane parallel
- * to a cardinal plane.
- */
-Matrix!(T,4) reflectionMatrix(T) (Axis reflectionAxis, T k)
-body
-{
-    auto res = Matrix!(T,4).identity;
+// TODO: reflectionMatrix
 
-    switch (reflectionAxis)
-    {
-        case Axis.x:
-            res.a11 = -1.0; res.a21 =  0.0; res.a31 =  0.0; res.a41 = 2.0 * k;
-            res.a12 =  0.0; res.a22 =  1.0; res.a32 =  0.0; res.a42 = 0.0;
-            res.a13 =  0.0; res.a23 =  0.0; res.a33 =  1.0; res.a43 = 0.0;
-            break;
-
-        case Axis.y:
-            res.a11 =  1.0; res.a21 =  0.0; res.a31 =  0.0; res.a41 = 0.0;
-            res.a12 =  0.0; res.a22 = -1.0; res.a32 =  0.0; res.a42 = 2.0 * k;
-            res.a13 =  0.0; res.a23 =  0.0; res.a33 =  1.0; res.a43 = 0.0;
-            break;
-
-        case Axis.z:
-            res.a11 =  1.0; res.a21 =  0.0; res.a31 =  0.0; res.a41 = 0.0;
-            res.a12 =  0.0; res.a22 =  1.0; res.a32 =  0.0; res.a42 = 0.0;
-            res.a13 =  0.0; res.a23 =  0.0; res.a33 = -1.0; res.a43 = 2.0 * k;
-            break;
-
-        default:
-            assert(0);
-    }
-
-    return res;
-}
-
-/*
- * Setup the matrix to perform a reflection about an arbitrary plane
- * through the origin.  The unit vector n is perpendicular to the plane.
- */
-Matrix!(T,4) axisReflectionMatrix(T) (Vector!(T,3) n)
-in
-{
-    assert (fabs(dot(n, n) - 1.0) < 0.001);
-}
-body
-{
-    auto res = Matrix!(T,4).identity;
-
-    T ax = -2.0 * n.x;
-    T ay = -2.0 * n.y;
-    T az = -2.0 * n.z;
-
-    res.a11 = 1.0 + (ax * n.x);
-    res.a22 = 1.0 + (ay * n.y);
-    res.a32 = 1.0 + (az * n.z);
-
-    res.a12 = res.a21 = (ax * n.y);
-    res.a13 = res.a31 = (ax * n.z);
-    res.a23 = res.a32 = (ay * n.z);
-
-    return res;
-}
+// TODO: axisReflectionMatrix
 
 /*
  * Setup the matrix to perform a "Look At" transformation 
@@ -396,46 +285,7 @@ body
     return (rot * trans);
 }
 
-/*
- * Setup a frustum matrix given the left, right, bottom, top, near, and far
- * values for the frustum boundaries.
- */
-Matrix!(T,4) frustumMatrix(T) (T l, T r, T b, T t, T n, T f)
-in
-{
-    assert (n >= 0.0);
-    assert (f >= 0.0);
-}
-body
-{
-    auto res = Matrix!(T,4).identity;
-
-    T width  = r - l;
-    T height = t - b;
-    T depth  = f - n;
-
-    res.arrayof[0] = (2 * n) / width;
-    res.arrayof[1] = 0.0;
-    res.arrayof[2] = 0.0;
-    res.arrayof[3] = 0.0;
-
-    res.arrayof[4] = 0.0;
-    res.arrayof[5] = (2 * n) / height;
-    res.arrayof[6] = 0.0;
-    res.arrayof[7] = 0.0;
-
-    res.arrayof[8] = (r + l) / width;
-    res.arrayof[9] = (t + b) / height;
-    res.arrayof[10]= -(f + n) / depth;
-    res.arrayof[11]= -1.0;
-
-    res.arrayof[12]= 0.0;
-    res.arrayof[13]= 0.0;
-    res.arrayof[14]= -(2 * f * n) / depth;
-    res.arrayof[15]= 0.0;
-
-    return res;
-}
+// TODO: frustumMatrix
 
 /*
  * Setup a perspective matrix given the field-of-view in the Y direction
@@ -467,51 +317,17 @@ body
     res.arrayof[8] = 0.0;
     res.arrayof[9] = 0.0;
     res.arrayof[10]= -(f + n) / (f - n);
-    res.arrayof[11]= -1.0;
+    res.arrayof[11]= -1.0f; //-(2 * f * n) / (f - n);
 
     res.arrayof[12]= 0.0;
     res.arrayof[13]= 0.0;
-    res.arrayof[14]= -(2 * f * n) / (f - n);
+    res.arrayof[14]= -(2 * f * n) / (f - n); //-1.0;
     res.arrayof[15]= 0.0;
 
     return res;
 }
 
-/*
- * Setup an orthographic Matrix4x4 given the left, right, bottom, top, near,
- * and far values for the frustum boundaries.
- */
-Matrix!(T,4) orthoMatrix(T) (T l, T r, T b, T t, T n, T f)
-body
-{
-    auto res = Matrix!(T,4).identity;
-
-    T width  = r - l;
-    T height = t - b;
-    T depth  = f - n;
-
-    res.arrayof[0] =  2.0 / width;
-    res.arrayof[1] =  0.0;
-    res.arrayof[2] =  0.0;
-    res.arrayof[3] =  0.0;
-
-    res.arrayof[4] =  0.0;
-    res.arrayof[5] =  2.0 / height;
-    res.arrayof[6] =  0.0;
-    res.arrayof[7] =  0.0;
-
-    res.arrayof[8] =  0.0;
-    res.arrayof[9] =  0.0;
-    res.arrayof[10]= -2.0 / depth;
-    res.arrayof[11]=  0.0;
-
-    res.arrayof[12]= -(r + l) / width;
-    res.arrayof[13]= -(t + b) / height;
-    res.arrayof[14]= -(f + n) / depth;
-    res.arrayof[15]=  1.0;
-
-    return res;
-}
+// TODO: orthoMatrix
 
 /*
  * Setup an orientation matrix using 3 basis normalized vectors
@@ -529,79 +345,11 @@ body
     return res;
 }
 
-/*
- * Setup a matrix that flattens geometry into a plane, 
- * as if it were casting a shadow from a light
- */
-Matrix!(T,4) shadowMatrix(T) (Vector!(T,4) groundplane, Vector!(T,4) lightpos)
-{
-    T d = dot(groundplane, lightpos);
+// TODO: shadowMatrix
 
-    auto res = Matrix!(T,4).identity;
+// TODO: directionToMatrix
 
-    res.a11 = d-lightpos.x * groundplane.x;
-    res.a21 =  -lightpos.x * groundplane.y;
-    res.a31 =  -lightpos.x * groundplane.z;
-    res.a41 =  -lightpos.x * groundplane.w;
-
-    res.a12 =  -lightpos.y * groundplane.x;
-    res.a22 = d-lightpos.y * groundplane.y;
-    res.a32 =  -lightpos.y * groundplane.z;
-    res.a42 =  -lightpos.y * groundplane.w;
-
-    res.a13 =  -lightpos.z * groundplane.x;
-    res.a23 =  -lightpos.z * groundplane.y;
-    res.a33 = d-lightpos.z * groundplane.z;
-    res.a43 =  -lightpos.z * groundplane.w;
-
-    res.a14 =  -lightpos.w * groundplane.x;
-    res.a24 =  -lightpos.w * groundplane.y;
-    res.a34 =  -lightpos.w * groundplane.z;
-    res.a44 = d-lightpos.w * groundplane.w;
-    
-    return res;
-}
-
-/*
- * Setup an orientation matrix using forward direction vector
- */
-Matrix!(T,4) directionToMatrix(T) (Vector!(T,3) zdir)
-{
-    Vector!(T,3) xdir = Vector!(T,3)(0.0, 0.0, 1.0);
-    Vector!(T,3) ydir;
-    float d = zdir.z;
-
-    if (d > -0.999999999 && d < 0.999999999)
-    {
-        xdir = xdir - zdir * d;
-        xdir.normalize();
-        ydir = cross(zdir, xdir);
-    }
-    else
-    {
-        xdir = Vector!(T,3)(zdir.z, 0.0, -zdir.x);
-        ydir = Vector!(T,3)(0.0, 1.0, 0.0);
-    }
-
-    Matrix4x4!(T) m = identityMatrix4x4!T();
-    m.forward = zdir;
-    m.right = xdir;
-    m.up = ydir;
-
-    return m;
-}
-
-/*
- * Setup an orientation matrix that performs rotation
- * between two vectors 
- */
-Matrix!(T,4) rotationBetweenVectors(T) (Vector!(T,3) source, Vector!(T,3) target)
-{
-    T d = dot(source, target);
-    Vector!(T,3) c = cross(source, target);
-    c.normalize();
-    return matrixFromAxisAngle(c, acos(d));
-}
+// TODO: rotationBetweenVectors
 
 /*
  * Transformations in 2D space
@@ -612,19 +360,58 @@ body
     Matrix!(T,2) res;
     T s = sin(theta);
     T c = cos(theta);
-    res.a11 = c; res.a21 = -s;
-    res.a12 = s; res.a22 = c;
+    res.a11 = c;  res.a12 = s;
+    res.a21 = -s; res.a22 = c;
     return res;
 }
 
+// TODO: generalize for arbitrary dimension
+// TODO: probably move to dlib.math.vector
 Matrix!(T,2) tensorProduct(T) (Vector!(T,2) u, Vector!(T,2) v)
 body
 {
     Matrix!(T,2) res;
     res[0] = u[0] * v[0];
-    res[1] = u[0] * v[1];
-    res[2] = u[1] * v[0];
+    res[1] = u[1] * v[0];
+    res[2] = u[0] * v[1];
     res[3] = u[1] * v[1];
     return res;
 }
 
+alias tensorProduct outerProduct;
+
+unittest
+{
+    bool isAlmostZero(Vector4f v)
+    {
+        float e = 0.002f;
+        
+        return abs(v.x) < e &&
+               abs(v.y) < e &&
+               abs(v.z) < e &&
+               abs(v.w) < e;
+    }
+
+    // build ModelView (World to Camera)    
+    vec3 center = vec3(0.0f, 0.0f, 0.0f);
+    vec3 eye = center + vec3(0.0f, 1.0f, 1.0f);
+    vec3 up = vec3(0.0f, -0.707f, 0.707f);
+
+    Matrix4f modelView = lookAtMatrix(eye, center, up);
+    
+    // build Projection (Camera to Eye)
+    Matrix4f projection = perspectiveMatrix(45.0f, 16.0f / 9.0f, 1.0f, 100.0f);
+    
+    // compose into one transformation
+    Matrix4f projectionModelView = projection * modelView;
+    
+    vec4 positionInWorld = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    vec4 transformed1 =
+        positionInWorld * projectionModelView;
+        
+    vec4 transformed2 =
+        (positionInWorld * modelView) * projection;
+        
+    assert(isAlmostZero(transformed1 - transformed2));
+}
