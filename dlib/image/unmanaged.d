52806d6e0a5c43e5d16ca96e32ede3425b3717a8
Timur Gafarov <gecko0307@gmail.com>
2015-06-12 20:40:27 +0400

    Fixed #64

--- dlib/image/unmanaged.d
+++ dlib/image/unmanaged.d
@@ -26,7 +26,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-module dlib.core.image;
+module dlib.image.unmanaged;
 
 import dlib.image.image;
 import dlib.core.memory;
