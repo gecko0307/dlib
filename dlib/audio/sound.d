fb7b29de47d928c0b31bfa6fe24ef7ac3a1ab99c
Timur Gafarov <gecko0307@gmail.com>
2015-12-05 00:09:14 +0300

    Fixed wrong module name

--- dlib/audio/sound.d
+++ dlib/audio/sound.d
@@ -26,7 +26,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
 
-module audio.sound;
+module dlib.audio.sound;
 
 import std.math;
 import dlib.audio.sample;
