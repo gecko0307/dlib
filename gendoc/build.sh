a25b6f5509ee2656574d1548bf92f1362eb68481
Timur Gafarov <gecko0307@gmail.com>
2015-12-16 14:26:03 +0300

    Generate HTML in UTF-8 encoding

--- gendoc/build.sh
+++ gendoc/build.sh
@@ -3,7 +3,7 @@ git clone git@github.com:gecko0307/dlib.wiki.git
 cd dlib.wiki
 for i in *.md; do 
     MD="$(python ../markdown2.py --extras cuddled-lists,link-patterns,fenced-code-blocks --link-patterns-file ../linkpatterns --html4tags $i)";
-    HTML="<html><head><link rel=\"stylesheet\" href=\"github-markdown.css\"><link rel=\"stylesheet\" href=\"code-default.css\"></head><body class=\"markdown-body\" style=\"margin-left: 20%; margin-right: 20%;\">${MD}</body></html>";
+    HTML="<html><head><meta charset=\"utf-8\"/><link rel=\"stylesheet\" href=\"github-markdown.css\"><link rel=\"stylesheet\" href=\"code-default.css\"></head><body class=\"markdown-body\" style=\"margin-left: 20%; margin-right: 20%;\">${MD}</body></html>";
     echo "${HTML}" > ../html/${i%.*}.html;
 done;
 cd ..
